540ffa34ea5ab77c08d9deb3d3d7b3c0b5966c59
Arowolo Abiodun <aarowolom@gmail.com>
2025-02-08 23:12:13 +0100

    add connection indicator

--- client/src/Controller.tsx
+++ client/src/Controller.tsx
@@ -4,21 +4,23 @@ import DisplayTime from "./DisplayTime";
 const Controller: React.FC = () => {
 	const [time, setTime] = useState<number>(0);
 	const [ws, setWs] = useState<WebSocket | null>(null);
-	const [isRunning, setIsRunning] = useState<boolean>(false);
+	const [isRunning, setIsRunning] = useState<boolean>(true);
+	const [isConnected, setIsConnected] = useState<boolean>(false);
 	const wsHost = import.meta.env.VITE_WS_HOST || "0.0.0.0";
-	const wsPort = import.meta.env.VITE_WS_PORT || 8080;
+	const wsPort = import.meta.env.VITE_WS_PORT || 8085;
 	const wsUrl = `ws://${wsHost}:${wsPort}`;
 
+	console.log(isRunning);
 	useEffect(() => {
 		const socket = new WebSocket(wsUrl);
 		setWs(socket);
 
 		socket.onopen = () => {
-			console.log("Connected to WebSocket server");
+			setIsConnected(true);
 		};
 
 		socket.onclose = () => {
-			console.log("Disconnected from WebSocket server");
+			setIsConnected(false);
 		};
 
 		socket.onmessage = (event) => {
@@ -31,7 +33,7 @@ const Controller: React.FC = () => {
 		return () => {
 			socket.close();
 		};
-	}, []);
+	}, [wsUrl]);
 
 	const sendTime = (increment: number) => {
 		if (ws && ws.readyState === WebSocket.OPEN) {
@@ -67,7 +69,18 @@ const Controller: React.FC = () => {
 
 	return (
 		<div className="d-flex flex-column justify-content-center align-items-center vh-100">
-			<DisplayTime />
+			<div
+				style={{
+					position: "fixed",
+					top: 10,
+					right: 10,
+					width: 20,
+					height: 20,
+					borderRadius: "50%",
+					backgroundColor: isConnected ? "green" : "red",
+				}}
+			/>
+			<DisplayTime size={2} />
 			<div className="position-absolute bottom-0 w-100 d-flex justify-content-between p-3">
 				<button
 					className="btn btn-lg btn-secondary flex-fill mx-1"
@@ -91,8 +104,7 @@ const Controller: React.FC = () => {
 					className={`btn btn-lg flex-fill mx-1 ${
 						isRunning ? "btn-danger" : "btn-success"
 					}`}
-					onClick={toggleTimer}
-					title={isRunning ? "Stop Timer" : "Start Timer"}>
+					onClick={toggleTimer}>
 					<i
 						className={`bi ${
 							isRunning ? "bi-pause-circle" : "bi-play-circle"
